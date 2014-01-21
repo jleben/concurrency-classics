04a7e98324f455c4e2fa1925f1b766b2733aff5b
Jakob Leben <jakob.leben@gmail.com>
2014-01-21 00:02:47 -0800

    h2o update

--- cpp/h2o.cpp
+++ cpp/h2o.cpp
@@ -1,3 +1,6 @@
+#define BERKELEY_SOLUTION 0
+#define UVIC_SOLUTION 1
+
 #include <thread>
 #include <mutex>
 #include <condition_variable>
@@ -9,165 +12,291 @@
 #include <cstdlib>
 #include <cstdint>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 using namespace std::chrono;
 
-class agent;
-
-class element
+struct element
 {
-protected:
+  bool bonded;
+  element():
+    bonded(false)
+  {}
+};
+
+struct hydrogen : public element
+{
+
+};
+
+struct oxygen : public element
+{
+
+};
+
+  // The official Berkeley solution does not ensure threads actually run
+  // in correct order (as opposed to become runnable in correct order).
+  // Consider the case where there is an abundance of H available, and two
+  // O come tightly one after another. The first O will succeed to acquire
+  // the mutex, and the second one will attempt to acquire it before any
+  // H is awaken. When an H is awaken, it needs to acquire the mutex, but the
+  // seconds O will get it before, continuing straight to "bond()" because
+  // there's plenty of H available.
+  // Thus, two Os will call "bond()" in succession.
+
+#if BERKELEY_SOLUTION
+class barrier
+{
+  int available_h;
+  int available_o;
+  int bonded_h;
+  int bonded_o;
+
+  condition_variable m_o_condition;
+  condition_variable m_h_condition;
+
+  string m_molecule;
+
   mutex m_mutex;
-  condition_variable m_signal;
-  agent *m_agent;
-  bool m_bonded;
-  thread m_thread;
 
-public:
-  element(agent *a):
-    m_agent(a),
-    m_bonded(false),
-    m_thread(&element::bond, this)
-  {}
-
-  void signal_bonded()
+  void bond(char element)
   {
-    m_mutex.lock();
-    m_bonded = true;
-    m_mutex.unlock();
-    m_signal.notify_all();
+    m_molecule += element;
+
+    if (m_molecule.size() == 3)
+    {
+      cout << m_molecule;
+      if (std::count(m_molecule.begin(), m_molecule.end(), 'H') != 2)
+        cout << " ERROR!";
+      cout << endl;
+
+      m_molecule.clear();
+    }
   }
 
+public:
+  barrier():
+    available_h(0),
+    available_o(0),
+    bonded_h(0),
+    bonded_o(0)
+  {}
 
-  void join() { m_thread.join(); }
+  void process_hydrogen()
+  {
+    unique_lock<mutex> lock(m_mutex);
 
-protected:
-  virtual void bond() = 0;
+    available_h++;
+
+    // while not allowed to leave
+    while (bonded_h == 0) {
+      // try to make a water molecule
+      if (available_h >= 2 && available_o >= 1) {
+        available_h-=2; bonded_h+=2;
+        available_o-=1; bonded_o+=1;
+        m_h_condition.notify_one();
+        m_o_condition.notify_one();
+      }
+      // else wait for somebody else to
+      else {
+        m_h_condition.wait(lock);
+      }
+    }
+    bonded_h--;
+    bond('H');
+  }
+
+  void process_oxygen()
+  {
+    unique_lock<mutex> lock(m_mutex);
+
+    available_o++;
+
+    // while not allowed to leave
+    while (bonded_o == 0) {
+      // try to make a water molecule
+      if (available_h >= 2 && available_o >= 1) {
+        available_h-=2; bonded_h+=2;
+        available_o-=1; bonded_o+=1;
+        m_h_condition.notify_one();
+        m_h_condition.notify_one();
+      }
+      // else wait for somebody else to
+      else {
+        m_o_condition.wait(lock);
+      }
+    }
+    bonded_o--;
+    bond('O');
+  }
 };
+#endif
 
-class hydrogen;
-class oxygen;
 
-class agent
+#if UVIC_SOLUTION
+
+struct barrier
 {
   mutex m_mutex;
-  condition_variable m_signal;
-  queue<hydrogen*> m_h_queue;
-  queue<oxygen*> m_o_queue;
-  thread m_thread;
+
+  condition_variable m_stage_gate;
+  condition_variable m_bond_gate;
+
+  int staged_h;
+  int staged_o;
+
+  uint64_t queued_h;
+  uint64_t queued_o;
+
+  string m_molecule;
 
 public:
-  agent():
-    m_thread(&agent::run, this)
+
+  barrier():
+    staged_h(0),
+    staged_o(0),
+    queued_h(0),
+    queued_o(0)
   {}
 
-  void bond(hydrogen *h)
+  bool stage_complete()
   {
-    unique_lock<mutex> lock(m_mutex);
-    m_h_queue.push(h);
-    m_signal.notify_all();
+    return staged_h == 2 && staged_o == 1;
   }
 
-  void bond(oxygen *o)
+  bool molecule_complete()
   {
-    unique_lock<mutex> lock(m_mutex);
-    m_o_queue.push(o);
-    m_signal.notify_all();
+    return m_molecule.size() == 3;
   }
 
-  void join() { m_thread.join(); }
+  void release_molecule()
+  {
+    cout << m_molecule;
+    if (std::count(m_molecule.begin(), m_molecule.end(), 'H') != 2)
+      cout << " ERROR!";
+    cout << endl;
 
-private:
-  void run();
+    m_molecule.clear();
+  }
+
+  void process_hydrogen()
+  {
+    bool queued = false;
+
+    unique_lock<mutex> lock(m_mutex);
+
+    while (staged_h == 2)
+    {
+      if (!queued)
+      {
+        ++queued_h;
+        //cout << "queued H = " << queued_h << endl;
+      }
+      queued = true;
+      m_stage_gate.wait(lock);
+    }
+
+    if (queued)
+      --queued_h;
+
+    ++staged_h;
+    //cout << "staged H" << endl;
+
+    while (!stage_complete())
+      m_bond_gate.wait(lock);
+
+    m_bond_gate.notify_all();
+
+    m_molecule += 'H';
+    //cout << "bonded H" << endl;
+
+    if (molecule_complete())
+    {
+      release_molecule();
+      staged_h = 0;
+      staged_o = 0;
+      m_stage_gate.notify_all();
+    }
+  }
+
+  void process_oxygen()
+  {
+    bool queued = false;
+
+    unique_lock<mutex> lock(m_mutex);
+
+    while (staged_o == 1)
+    {
+      if (!queued)
+      {
+        ++queued_o;
+        //cout << "queued O = " << queued_o << endl;
+      }
+      queued = true;
+      m_stage_gate.wait(lock);
+    }
+
+    if (queued)
+      --queued_o;
+
+    ++staged_o;
+    //cout << "staged O" << endl;
+
+    while (!stage_complete())
+      m_bond_gate.wait(lock);
+
+    m_bond_gate.notify_all();
+
+    m_molecule += 'O';
+    //cout << "bonded O" << endl;
+
+    if (molecule_complete())
+    {
+      release_molecule();
+      staged_h = 0;
+      staged_o = 0;
+      m_stage_gate.notify_all();
+    }
+  }
 };
-
-class hydrogen : public element
-{
-public:
-  hydrogen(agent * a):
-    element(a)
-  {}
-
-private:
-  void bond()
-  {
-    cout << "+ H" << endl;
-    m_agent->bond(this);
-    unique_lock<mutex> lock(m_mutex);
-    if (!m_bonded)
-      m_signal.wait(lock, [&](){ return m_bonded; });
-    cout << "- H" << endl;
-  }
-};
-
-class oxygen : public element
-{
-public:
-  oxygen(agent * a):
-    element(a)
-  {}
-
-private:
-  void bond()
-  {
-    cout << "+ O" << endl;
-    m_agent->bond(this);
-    unique_lock<mutex> lock(m_mutex);
-    if (!m_bonded)
-      m_signal.wait(lock, [&](){ return m_bonded; });
-    cout << "- O" << endl;
-  }
-};
-
-
-void agent::run()
-{
-  auto can_bond = [&]()
-  {
-    return m_h_queue.size() >= 2 && m_o_queue.size() >= 1;
-  };
-
-  unique_lock<mutex> lock(m_mutex);
-  while(true)
-  {
-    if (!can_bond())
-      m_signal.wait(lock, can_bond);
-
-    hydrogen *h1 = m_h_queue.front();
-    m_h_queue.pop();
-    hydrogen *h2 = m_h_queue.front();
-    m_h_queue.pop();
-    oxygen *o2 = m_o_queue.front();
-    m_o_queue.pop();
-
-    h1->signal_bonded();
-    h2->signal_bonded();
-    o2->signal_bonded();
-
-    h1->join();
-    h2->join();
-    o2->join();
-  }
-}
+#endif
 
 int main()
 {
-  agent heisenberg;
+  barrier b;
 
   minstd_rand r;
   uniform_int_distribution<int> u(0,2);
 
   minstd_rand r2;
-  uniform_int_distribution<int> u2(50,100);
+  uniform_int_distribution<int> u2(10, 30);
+
+  int count = 10000;
+
   while(true)
   {
-    int choice = u(r);
-    if (choice == 2)
-      new oxygen(&heisenberg);
-    else
-      new hydrogen(&heisenberg);
+    cout << "------------------" << endl;
 
-    this_thread::sleep_for(milliseconds(u2(r)));
+    int batch_count = 50;
+    while( batch_count-- )
+    {
+      int choice = u(r);
+      if (choice == 2)
+      {
+        thread t( &barrier::process_oxygen, &b );
+        t.detach();
+      }
+      else
+      {
+        thread t( &barrier::process_hydrogen, &b );
+        t.detach();
+      }
+    }
+    //this_thread::sleep_for(milliseconds(u2(r)));
+    this_thread::sleep_for(milliseconds(500));
   }
+
+  cout << "ending..." << endl;
+  this_thread::sleep_for(milliseconds(100));
 }
