555768aff59901e101905bc8098e55504afafe20
Jakob Leben <jakob.leben@gmail.com>
2014-01-27 18:23:18 -0800

    util: dummy worker

--- cpp/util.hpp
+++ cpp/util.hpp
@@ -1,6 +1,7 @@
 #include <mutex>
 #include <atomic>
 #include <iostream>
+#include <random>
 
 class printer
 {
@@ -36,3 +37,21 @@ public:
 #else
 #define PRINT(x) printer() << x
 #endif
+
+class dummy_worker
+{
+  std::random_device rd;
+  std::minstd_rand R;
+  long count;
+public:
+  dummy_worker(long count = 1e3): R(rd()), count(count) {}
+  void work()
+  {
+    int c = count;
+    int result;
+    while(c--)
+    {
+      result = R() * R();
+    }
+  }
+};
